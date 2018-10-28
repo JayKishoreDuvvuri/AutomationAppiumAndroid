e939b38530a507c5b85981ddcd84251815e8a8fb
JayKishoreDuvvuri <42520193+JayKishoreDuvvuri@users.noreply.github.com>
2018-10-29 00:32:29 +0530

    Update CalculaMediaFinal.java

--- src/com/Automation/Apps/CalculaMediaFinal.java
+++ src/com/Automation/Apps/CalculaMediaFinal.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
